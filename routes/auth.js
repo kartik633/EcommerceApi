0c9c7e50c1e149fa1d4cd09257444c9e4976624a
Kartik <9415kks@gmail.com>
2021-11-18 23:21:09 +0530

    Added jwt access token

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJs = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 //Register
 router.post("/register",async (req,res)=>{
     const newUser = new User({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password,
+        password:CryptoJs.AES.encrypt(req.body.password,process.env.PASS_SEC).toString(),
     });
 
     try{
@@ -20,6 +22,37 @@ router.post("/register",async (req,res)=>{
     }
 
 });
+
+//Login
+router.post("/login", async(req,res) =>{
+    try{    
+        const user = await User.findOne({username:req.body.username});
+
+        !user && res.status(401).json("Wrong Credentials");
+
+        const hashedPassword = CryptoJs.AES.decrypt(user.password,process.env.PASS_SEC);
+
+        const OriginalPassword = hashedPassword.toString(CryptoJs.enc.Utf8);
+
+        OriginalPassword !==req.body.password && res.status(401).json("Wrong Credentials");
+
+        const accessToken  = jwt.sign(
+            {
+                id:user._id,
+                isAdmin:user.isAdmin,
+            }, 
+            process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
+
+        const {password, ...others} = user._doc;
+
+        res.status(200).json({others, accessToken});
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 
 
 
